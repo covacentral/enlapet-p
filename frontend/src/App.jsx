9a94229876b002996ed3c3b87432db349082a30e
Daniel Robles <covacentral@gmail.com>
2026-04-24 10:03:58 -0500

    fix: Scaffold basic Vite files to fix Vercel build

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Enlapet 2.0</h1>
+      <p>El Frontend se ha desplegado correctamente y está listo para la Fase 2 🐾.</p>
+    </div>
+  );
+}
+
+export default App;
